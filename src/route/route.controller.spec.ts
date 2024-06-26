43e99e3affcd360293f3320a0aa1ab824ac0edc6
Yuta-Dohmae <yuta.doumae@goodsun-hd.com>
2024-06-26 11:03:55 +0900

    test_in_jest

--- src/route/route.controller.spec.ts
+++ src/route/route.controller.spec.ts
@@ -1,5 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RouteController } from './route.controller';
+import * as puppeteer from 'puppeteer';
+
+jest.mock('puppeteer');
 
 describe('RouteController', () => {
   let controller: RouteController;
@@ -14,5 +17,46 @@ describe('RouteController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('success', async () => {
+    const mockPage = {
+      goto: jest.fn(),
+      evaluate: jest.fn().mockResolvedValue({
+        duration: '07:42発→08:27着45分(乗車21分)',
+        fare: 'IC優先:209円',
+        pass: '定期券 通勤:1か月 8,170円 / 3か月 23,290円 / 6か月 44,120円',
+      }),
+    };
+
+    const mockBrowser = {
+      newPage: jest.fn().mockResolvedValue(mockPage),
+      close: jest.fn(),
+    };
+
+    (puppeteer.launch as jest.Mock).mockResolvedValue(mockBrowser);
+
+    const result = await controller.search(
+      '新中野',
+      '神楽坂アプリケーション総研株式会社',
+      '1',
+    );
+
+    const expectedUrl = `https://transit.yahoo.co.jp/search/print?from=新中野&to=神楽坂アプリケーション総研株式会社&y=2024&m=06&d=26&hh=08&m1=3&m2=0&type=4&s=1`;
+
+    console.log(result);
+
+    expect(puppeteer.launch).toHaveBeenCalled();
+    expect(mockBrowser.newPage).toHaveBeenCalled();
+    expect(mockPage.goto).toHaveBeenCalledWith(expectedUrl, {
+      waitUntil: 'networkidle2',
+    });
+    expect(mockPage.evaluate).toHaveBeenCalled();
+    expect(mockBrowser.close).toHaveBeenCalled();
+    expect(result).toEqual({
+      duration: '07:42発→08:27着45分(乗車21分)',
+      fare: 'IC優先:209円',
+      pass: '定期券 通勤:1か月 8,170円 / 3か月 23,290円 / 6か月 44,120円',
+    });
   });
 });
--- src/search/search.controller.spec.ts
+++ src/search/search.controller.spec.ts
@@ -1,10 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SearchController } from './search.controller';
+import axios from 'axios';
+
+jest.mock('axios');
 
 describe('SearchController', () => {
   let controller: SearchController;
+  let originalEnv: NodeJS.ProcessEnv;
+
+  beforeAll(() => {
+    originalEnv = process.env;
+  });
 
   beforeEach(async () => {
+    process.env = {
+      ...originalEnv,
+      GOOGLE_API_KEY: 'apikey',
+      GOOGLE_SEARCH_ENGINE_ID: 'id',
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SearchController],
     }).compile();
@@ -12,7 +26,48 @@ describe('SearchController', () => {
     controller = module.get<SearchController>(SearchController);
   });
 
+  afterAll(() => {
+    process.env = originalEnv;
+  });
+
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('success', async () => {
+    const mockResponse = {
+      data: {
+        items: [
+          { title: 'Test1', link: 'https://example.com/1' },
+          { title: 'Test2', link: 'https://example.com/2' },
+        ],
+      },
+    };
+
+    (axios.get as jest.Mock).mockResolvedValue(mockResponse);
+
+    const result = await controller.search('query');
+
+    const expectedUrl = `https://www.googleapis.com/customsearch/v1?key=apikey&cx=id&q=query`;
+    expect(axios.get).toHaveBeenCalledWith(expectedUrl);
+    expect(result).toEqual(mockResponse.data);
+  });
+
+  it('APIKEY error', async () => {
+    process.env.GOOGLE_API_KEY = '';
+    process.env.GOOGLE_SEARCH_ENGINE_ID = '';
+
+    await expect(controller.search('query')).rejects.toThrow(
+      'Google API key and Search Engine ID must be provided.',
+    );
+  });
+
+  it('request fails error', async () => {
+    const errorMessage = 'Network Error';
+    (axios.get as jest.Mock).mockRejectedValue(new Error(errorMessage));
+
+    await expect(controller.search('test-query')).rejects.toThrow(
+      `Error searching: ${errorMessage}`,
+    );
   });
 });
